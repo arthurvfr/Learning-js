edb2c24f75ad44451b113e35192f8b93913fdf0d
Arthur Vinicius <111816515+arthurvfr@users.noreply.github.com>
2023-09-13 20:42:35 -0300

    first steps

---
+++ fundamentos.js
@@ -0,0 +1,32 @@
+var minhaVariavel = "Ola, Mundo!";
+
+console.log(minhaVariavel);
+
+var meuNumero = 10;
+
+console.log(meuNumero);
+
+console.log(meuNumero + 5);
+
+console.log("10" + 5);
+
+console.log(typeof meuNumero);
+
+var meuBooleano = true; //false
+
+console.log(typeof meuBooleano);
+
+var meuObjeto = {};
+var meuArray = {};
+var meuNull = null;
+var meuUndefined = undefined;
+
+console.log(typeof meuObjeto);
+console.log(typeof meuArray);
+console.log(typeof meuNull);
+console.log(typeof meuUndefined);
+
+//let e const
+//novas formas de declarar vairaveis
+let x = 10;
+const y = 5;
